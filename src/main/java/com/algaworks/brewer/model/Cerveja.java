9c89246c4c46ccbecf912d9376df2f81d7ceb305
Danusio Diniz <danusiodiniz@gmail.com>
2018-07-31 15:04:16 -0300

    Projeto Criado

---
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -0,0 +1,42 @@
+package com.algaworks.brewer.model;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Cerveja {
+
+	@NotBlank(message = "SKU é obrigatório")
+	private String sku;
+	
+	@NotBlank(message = "Nome é obrigatório")
+	private String nome;
+	
+	@Size(min=1, max=50, message = "Descrição deve ter no máximo 50 caracteres")
+	private String descricao;
+	
+	public String getSku() {
+		return sku;
+	}
+	
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+}
